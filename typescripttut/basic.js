1630135c72d89322b18465d213a46c3e6e42651e
Priya Agrasen <priyaagrasencse@gmail.com>
2026-01-11 14:59:42 +0530

    initial commit

---
+++ typescripttut/basic.js
@@ -0,0 +1,7 @@
+function greet(name) {
+    return "Hello", name;
+}
+console.log(greet("hitesh"));
+// but if we change the data type inside it will give error
+console.log(greet(48));
+// so we use typescript
---
+++ typescripttut/javascript/one.js
@@ -0,0 +1,5 @@
+console.log("Priya")
+// both js and txt files can be used for source code
+//
+
+
